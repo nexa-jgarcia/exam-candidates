b9c2694f299b77a31f6ba3fcdeb1a5c771011793
Jonas Garcia <jonas.garcia@nexaminds.ai>
2026-02-05 13:15:07 -0600

    add vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/nexa-jgarcia.github.io/exam-candidates/',
+  base: '/exam-candidates/',
   plugins: [react()],
   server: {
     port: 5173,
